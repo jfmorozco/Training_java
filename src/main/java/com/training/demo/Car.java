be1d7d08c96616082efebba9cb3e4bc4ec4381e2
jfmorozco <jfmorozco@outlook.com>
2020-12-22 00:40:56 -0500

    my first commit

---
+++ src/main/java/com/training/demo/Car.java
@@ -0,0 +1,15 @@
+package com.training.demo;
+
+public class Car {
+    private float speed;
+    private String color;
+
+    public Car(float speed, String color){
+        this.speed = speed;
+        this.color = color;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+}
